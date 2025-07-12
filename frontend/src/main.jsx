9a91dc4992bc7db6b159dbc1fcf3b7e22fd7c3c4
cruzguille <guillermobelizcruz98@gmail.com>
2025-07-11 23:54:20 -0300

    ♻️ Código listo para deploy en Render

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,24 +5,20 @@ import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 
-// Detectar si está corriendo en Electron
 const isElectron = window?.process?.type === 'renderer';
-
-// Detectar si accedés desde otro dispositivo (celular)
 const isLocalhost =
   window.location.hostname === 'localhost' ||
   window.location.hostname === '127.0.0.1';
 
-axios.defaults.baseURL = isElectron
-  ? 'http://127.0.0.1:3001' // para Electron
+const baseURL = isElectron
+  ? 'http://127.0.0.1:3001'
   : isLocalhost
-  ? 'http://localhost:3001' // para la PC
-  : 'http://192.168.100.44:3001'; // para el celular
+  ? 'http://localhost:3001'
+  : import.meta.env.VITE_API_URL || 'http://192.168.100.44:3001';
 
+axios.defaults.baseURL = baseURL;
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 
-
-// Renderizar app
 const root = createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
